2bbd6f614a1246e4339e8cd24a59fe275c67cb22
fedeemilo <fedeemilo91@gmail.com>
2021-03-20 14:21:59 -0300

    start configuring mikroOrm

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
-console.log("hello world");
- 
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "clonereddit",
+    user: "milonitico",
+    password: "gracias2021!",
+    debug: !__prod__,
+    type: "postgresql"
+  });
+
+  const post = orm.em.create(Post, {title: 'my first post'})
+};
+
+main();
